5b924ec4ce0a80cb44f836ac159285eca6f8f97b
dãouanvinh992000 <ntchi75ns@gmail.com>
2020-03-12 19:59:14 +0700

    shop cover

---
+++ shopbanhang/vinh.js
@@ -0,0 +1,20 @@
+//alert('Chào mừng đến với shop Vinh đẹp trai chúc bạn 1 ngày may mắn');
+function myfun(){
+    var a=document.getElementById("main");
+    b=document.getElementById("javascript")
+    a.style.display="none";
+    b.style.display="block";
+}
+var person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function(){
+      return this.firstName + " " + this.lastName;
+    }
+  };
+  var a=[];
+  var n=6;
+  for(var i=0;i<n;i++){
+      
+  }
